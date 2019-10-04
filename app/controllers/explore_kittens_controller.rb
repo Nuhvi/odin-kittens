020a5020fa1e35fb0c4586e072662ca7b0ed644d
Nazeh <ar.nazeh@gmail.com>
2019-10-04 20:37:20 +0300

    finish flickr api

--- app/controllers/explore_kittens_controller.rb
+++ app/controllers/explore_kittens_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class ExploreKittensController < ApplicationController
-  def index; end
+  def index
+    @photos = Flickr.photos.search(tags: 'kittens').sample(10)
+  end
 end
