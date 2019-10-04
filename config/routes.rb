498ef8d6d1941c811805ce92fc701aa5031096e3
Nazeh <ar.nazeh@gmail.com>
2019-10-04 16:00:37 +0300

    generate kittens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'kittens/index'
+  get 'kittens/show'
+  get 'kittens/new'
+  get 'kittens/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
