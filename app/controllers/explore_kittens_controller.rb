1395239f962c3b8768b3624e6dd7559b155d40ea
Nazeh <ar.nazeh@gmail.com>
2019-10-04 19:46:19 +0300

    initiate ExploreKittens controller

---
+++ app/controllers/explore_kittens_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class ExploreKittensController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :explore_kittens, only: [:index]
   resources :kittens
   root 'kittens#index'
 end
