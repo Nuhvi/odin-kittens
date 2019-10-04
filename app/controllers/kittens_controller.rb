44a7c1e10c9ee15614d2afb4f46af5eacf3811fc
Nazeh <ar.nazeh@gmail.com>
2019-10-04 16:51:01 +0300

    start editing kittens controller and views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,17 +1,31 @@
 # frozen_string_literal: true
 
 class KittensController < ApplicationController
-  def index; end
+  def index
+    @kittens = Kitten.all
+  end
 
-  def show; end
+  def show
+    @kitten = set_kitten
+  end
 
   def new; end
 
   def create; end
 
-  def edit; end
+  def edit
+    @kitten = set_kitten
+  end
 
   def update; end
 
-  def destroy; end
+  def destroy
+    @kitten = set_kitten
+  end
+
+  private
+
+  def set_kitten
+    Kitten.find(params[:id])
+  end
 end
