dad5966778b9947eaa4f9754498fded050ce5533
Nazeh <ar.nazeh@gmail.com>
2019-10-04 16:21:35 +0300

    set default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'kittens/index'
-  get 'kittens/show'
-  get 'kittens/new'
-  get 'kittens/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :kittens
+  root 'kittens#index'
 end
