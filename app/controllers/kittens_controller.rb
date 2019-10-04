04030d12927394c23b632a423b2b43abaac89958
Nazeh <ar.nazeh@gmail.com>
2019-10-04 18:17:00 +0300

    update kittens controller and views

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -9,23 +9,37 @@ class KittensController < ApplicationController
     @kitten = set_kitten
   end
 
-  def new; end
+  def new
+    @kitten = Kitten.new
+  end
 
-  def create; end
+  def create
+    Kitten.create(kitten_params)
+  end
 
   def edit
     @kitten = set_kitten
   end
 
-  def update; end
+  def update
+    @kitten = set_kitten
+    @kitten.update(kitten_params)
+  end
 
   def destroy
-    @kitten = set_kitten
+    if @kitten = set_kitten
+      @kitten.destroy
+      redirect_to root_url
+    end
   end
 
   private
 
   def set_kitten
     Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
   end
 end
